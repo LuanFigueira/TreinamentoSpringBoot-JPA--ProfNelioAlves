7378cbe51ba4154ba77429fe2fc1ac10c3955f59
LuanFigueira <luan.figueira.oliveira@gmail.com>
2024-11-08 12:03:01 -0300

    Criação da classe User e da classe UserResource

--- src/main/java/LuanFigueira/TreinamentoSpringBootJPA/JavaBackEndApplication.java
+++ src/main/java/LuanFigueira/TreinamentoSpringBootJPA/JavaBackEndApplication.java
@@ -1,4 +1,4 @@
-package LuanFigueira.JavaBackEnd;
+package LuanFigueira.TreinamentoSpringBootJPA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
