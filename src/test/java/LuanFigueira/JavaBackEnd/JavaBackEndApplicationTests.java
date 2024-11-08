b9bb8c47044c9c95de47977bed01a2433752bb16
LuanFigueira <luan.figueira.oliveira@gmail.com>
2024-11-08 11:20:24 -0300

    Criação do projeto

---
+++ src/test/java/LuanFigueira/JavaBackEnd/JavaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package LuanFigueira.JavaBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
