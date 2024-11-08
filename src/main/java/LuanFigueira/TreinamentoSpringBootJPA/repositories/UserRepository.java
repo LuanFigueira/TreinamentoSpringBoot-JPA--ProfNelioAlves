f86346d2ce2bdf9025fe4edd922be3bde4eaa821
LuanFigueira <luan.figueira.oliveira@gmail.com>
2024-11-08 14:51:26 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/LuanFigueira/TreinamentoSpringBootJPA/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package LuanFigueira.TreinamentoSpringBootJPA.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import LuanFigueira.TreinamentoSpringBootJPA.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+
+}
